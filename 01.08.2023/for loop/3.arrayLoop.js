b01558b6add6e7b40b77af72f2697b905431db74
akashseub <miahmdakash55@gmail.com>
2023-08-02 00:19:22 +0600

    array loop

---
+++ 01.08.2023/for loop/3.arrayLoop.js
@@ -0,0 +1,6 @@
+let numbers = [23, 24, 54, 234, 234, 23, 21];
+
+for(i=0; i<numbers.length; i++){
+    number = numbers[i];
+    console.log(number);
+}
--- 01.08.2023/while loop/4.odd.js
+++ 01.08.2023/while loop/4.odd.js
@@ -1,6 +1,6 @@
 let number = 1;
 
-while(number <=10){
+while(number <=100){
     if(number%2 != 0){
         console.log(number);
     }
